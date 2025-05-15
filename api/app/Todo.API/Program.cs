3c9bab23b89a6fe4e97f3f91e4c9425c5f9991c6
Vitalii Novik <novickvitaliy@gmail.com>
2025-05-15 16:53:07 +0300

    feat: add application layer

--- api/app/Todo.API/Program.cs
+++ api/app/Todo.API/Program.cs
@@ -1,4 +1,8 @@
+using Todo.Application;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.RegisterApplicationLayer(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ api/app/Todo.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.BussinessLayer;
+
+namespace Todo.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.RegisterBussinessLayer(configuration);
+
+        return services;
+    }
+}

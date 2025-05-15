b5a89170639f5bc134041cbb132d215a87b6df27
Vitalii Novik <novickvitaliy@gmail.com>
2025-05-15 16:49:31 +0300

    feat: add bussiness layer

---
+++ api/app/Todo.BussinessLayer/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.DataAccess;
+
+namespace Todo.BussinessLayer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterBussinessLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.RegisterDataAccessLayer(configuration);
+        
+        return services;
+    }
+}
